773d342abe9a10cbd5353a196ffec61dc7124e08
shashikant1102 <102590136+shashikant1102@users.noreply.github.com>
2022-03-30 13:00:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
+<!DOCTYPE html>
 <html>
 <body>
-<h1>Welcome to Webapps</h1>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <input type="submit" Value="Connect to MySQL Server & Insert data in a table">
+</form>
 <?php
-  echo "This is the first file";
- ?>                                         
-</body>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myedunet";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br>";
+$sql = "INSERT INTO student VALUES (4, 'Savani', 'Shivraj', '86565');";
+$sql .= "INSERT INTO Student VALUES (7, 'twinkal', 'karad', '55');";
+$sql .= "INSERT INTO Student VALUES (8, 'pooja', 'karad', '55');";
+$sql .= "INSERT INTO Student VALUES (9, 'komal', 'abad', '55');";
+$sql .= "INSERT INTO Student VALUES (10, 'shreya', 'abad', '55');";
+$sql .= "INSERT INTO Student VALUES (11, 'mayuri', 'avasari', '55')";
 </html>
